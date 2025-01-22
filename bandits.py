1dd95031dbce7355f1587f20fea450437236f54a
VirtiMehta <153837823+VirtiMehta@users.noreply.github.com>
2025-01-22 11:54:29 +0530

    Add files via upload

---
+++ bandits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+
+class Bandit:
+    def __init__(self, mean=0, stddev=1):
+        self.__mean = mean
+        self.__stddev = stddev
+
+    '''This method simulates pulling the lever of the bandit and returns the reward'''
+    def pullLever(self):
+        return np.random.normal(self.__mean, self.__stddev)
+
+
+# In[ ]:
+
+
+
+
